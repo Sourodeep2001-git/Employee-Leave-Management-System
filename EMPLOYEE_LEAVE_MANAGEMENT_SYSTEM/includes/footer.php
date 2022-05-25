a93eebc63bac5d16510f5788c17cd123c808f90e
Sourodeep Paul <57056047+Sourodeep2001@users.noreply.github.com>
2022-05-25 09:59:55 +0530

    Delete EMPLOYEE_LEAVE_MANAGEMENT_SYSTEM directory

--- EMPLOYEE_LEAVE_MANAGEMENT_SYSTEM/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-        <div class="footer-area">
-                <p>©Copyright-<?php echo date("Y");?> | Developed By ELMS </a></p>
-                <p> Email: elms2k22@gmail.com</p>
-                
-        </div>
-</footer>
